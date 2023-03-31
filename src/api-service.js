f6791a21ad0af7300e26e23a5bb798f474fc2570
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-03-31 05:21:54 +0300

    refactoring

--- src/api-service.js
+++ src/api-service.js
@@ -5,19 +5,33 @@ export default class ApiService {
     this.numberOfImages = 0;
     this.numberOfLoadedImages = 0;
   }
-  fetchImages() {
-    return fetch(
-      `https://pixabay.com/api/?key=34827531-46fe6f83c6cd16e6040b33d37&q=${this.serchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=3`
-    )
-      .then(response => response.json())
-      .then(images => {
-        this.numberOfImages = images.totalHits;
-        this.incrementNomberOfLoadedImages(images.hits.length);
-        this.incrementPage();
-        // console.log(this.numberOfLoadedImages);
-        return images;
-      })
-      .catch(console.error);
+  //   fetchImages() {
+  //     return fetch(
+  //       `https://pixabay.com/api/?key=34827531-46fe6f83c6cd16e6040b33d37&q=${this.serchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=3`
+  //     )
+  //       .then(response => response.json())
+  //       .then(images => {
+  //         this.numberOfImages = images.totalHits;
+  //         this.incrementNomberOfLoadedImages(images.hits.length);
+  //         this.incrementPage();
+  //         // console.log(this.numberOfLoadedImages);
+  //         return images;
+  //       })
+  //       .catch(console.error);
+  //   }
+
+  async fetchImages() {
+    const response = await fetch(
+      `https://pixabay.com/api/?key=34827531-46fe6f83c6cd16e6040b33d37&q=${this.serchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+    );
+    const images = await response.json();
+
+    this.numberOfImages = images.totalHits;
+    this.incrementNomberOfLoadedImages(images.hits.length);
+    this.incrementPage();
+    // console.log(this.numberOfLoadedImages);
+
+    return images;
   }
 
   incrementNomberOfLoadedImages(number) {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import InfiniteScroll from 'infinite-scroll';
 import ApiService from './api-service';
 
 const refs = {
@@ -12,11 +15,12 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 const apiService = new ApiService();
 
-function onSearch(e) {
+async function onSearch(e) {
   e.preventDefault();
 
   apiService.query = e.currentTarget.elements.searchQuery.value;
   refs.loadMoreBtn.classList.add('--is-hidden');
+
   if (apiService.query.trim() === '') {
     return Notiflix.Notify.failure(
       'Sorry, the search query should not be empty'
@@ -25,7 +29,9 @@ function onSearch(e) {
   apiService.resetPage();
   apiService.resetNumberOfLoadedImages();
 
-  apiService.fetchImages().then(images => {
+  const images = await apiService.fetchImages();
+
+  try {
     if (images.hits.length === 0) {
       refs.loadMoreBtn.classList.add('--is-hidden');
       return Notiflix.Notify.info(
@@ -36,14 +42,45 @@ function onSearch(e) {
     refs.loadMoreBtn.classList.remove('--is-hidden');
     clearGallery();
     renderQueryImageCard(images.hits);
-  });
+
+    Notiflix.Notify.info(`Hooray! We found ${images.totalHits} images.`);
+  } catch (error) {
+    console.error('caught error: ', error);
+  }
 }
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   apiService.query = e.currentTarget.elements.searchQuery.value;
+//   refs.loadMoreBtn.classList.add('--is-hidden');
+//   if (apiService.query.trim() === '') {
+//     return Notiflix.Notify.failure(
+//       'Sorry, the search query should not be empty'
+//     );
+//   }
+//   apiService.resetPage();
+//   apiService.resetNumberOfLoadedImages();
+
+//   apiService.fetchImages().then(images => {
+//     if (images.hits.length === 0) {
+//       refs.loadMoreBtn.classList.add('--is-hidden');
+//       return Notiflix.Notify.info(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     }
+
+//     refs.loadMoreBtn.classList.remove('--is-hidden');
+//     clearGallery();
+//     renderQueryImageCard(images.hits);
+//   });
+// }
 
 function renderQueryImageCard(arrOfImages) {
   const markup = arrOfImages
     .map(image => {
       return `<div class="photo-card">
-  <img width="370" heigth="270" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <a href="${image.largeImageURL}"><img width="370" heigth="270" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
       <b>${image.likes}</b>
@@ -63,19 +100,50 @@ function renderQueryImageCard(arrOfImages) {
     .join('');
 
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+  const { height: cardHeight } =
+    refs.gallery.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+
+  // let infScroll = new InfiniteScroll('.gallery', {
+  //   // options
+  //   path: '.pagination__next',
+  //   append: '.post',
+  //   history: false,
+  // });
 }
 
-function onLoadMore() {
-  apiService.fetchImages().then(images => {
-    if (apiService.numberOfLoadedImages >= apiService.numberOfImages) {
-      refs.loadMoreBtn.classList.add('--is-hidden');
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-    renderQueryImageCard(images.hits);
-  });
+async function onLoadMore() {
+  const images = await apiService.fetchImages();
+  if (apiService.numberOfLoadedImages >= apiService.numberOfImages) {
+    refs.loadMoreBtn.classList.add('--is-hidden');
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+  renderQueryImageCard(images.hits);
 }
+
+// function onLoadMore() {
+//   apiService.fetchImages().then(images => {
+//     if (apiService.numberOfLoadedImages >= apiService.numberOfImages) {
+//       refs.loadMoreBtn.classList.add('--is-hidden');
+//       Notiflix.Notify.info(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//     }
+//     renderQueryImageCard(images.hits);
+//   });
+// }
 
 function clearGallery() {
   refs.gallery.innerHTML = '';
